0dad98185b70e31647d0b4b5d80349b78b78d8e3
Adarsh Yagnik <adarsh_yagnik@yahoo.com>
2015-08-01 09:54:22 +0530

    Add search filter

--- api/controllers/BugController.js
+++ api/controllers/BugController.js
@@ -60,10 +60,14 @@ module.exports = {
 		q.status = req.param('status') || ['resolved', 'unresolved'];
 		var app = req.param('application');
 		var type = req.param('type');
+		var title = req.param('title');
+		var description = req.param('description');
 		var context = req.param('context');
 		if (app) q.application = app;
 		if (type) q.type = type;
 		if (context) q.context = context;
+		if (title) q.title = new RegExp('/' + title + '/' , 'gim');
+		if (description) q.description =  new RegExp('/' + description + '/' , 'gim');
 		Bug.find(q).sort({ status: -1, createdAt: -1 }).paginate({ page: page, limit : limit }).exec(function(err, bugs){
 			if (err) return next(err);
 			return res.view( { application: app, bugs: bugs, page: isNaN(page)?1:parseInt(page) });
